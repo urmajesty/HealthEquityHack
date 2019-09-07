4a01fab1d61874a2b7d5823cedaeffcf6f20a4ff
nicostran <nstranquist@gmail.com>
2019-09-07 16:25:31 -0500

    first commit

--- src/web_crawling/init.js
+++ src/web_crawling/init.js
@@ -1,20 +1,38 @@
 const puppeteer = require('puppeteer');
 const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
-const init =async (site) => { 
-    const browser = await puppeteer.launch();
+const init = async (site) => {
+    try {
+        const browser = await puppeteer.launch();
 
-    const page = await browser.newPage();
-    await page.goto(site);
+        const page = await browser.newPage();
+        await page.goto(site);
 
-    const html =await page.content();
-    const dom = await new jsdom(html);
+        const results = await page.content();
+        //console.log(results);
+        /*for(line in results) {
+            console.log(results[line]);
+            // 
+        }*/
 
-    console.log(dom);
-    await browser.close();
+        //global.DOMParser = new JSDOM().window.DOMParser
+        console.log(new JSDOM().window.DOMParser)
+        //const dom = (new JSDOM(results)).window.document.body;
+        //const dom = new JSDOM(results);
+
+        //console.log(dom);
+        // loop through dom and get text content
+        await browser.close();
+    } catch (err) {
+        console.log(err);
+    }
 }
 
-init('https://partnc.org');
+init('https://blog.walterkjenkins.com/190826-Hosting-a-Static-Site-on-s3/');
 
 
 
+} catch (err) {
+    console.log(err);
+}
