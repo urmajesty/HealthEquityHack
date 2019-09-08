f3b18a3df9a0ef3f45f626df401f999a1a365c20
CadillacJenkins <walter.k.jenkins@gmail.com>
2019-09-07 21:21:54 -0500

    finished web_crawling/load.py

--- src/web_crawling/load.py
+++ src/web_crawling/load.py
@@ -2,21 +2,22 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 urls = "https://www.partnc.org/"
-aList = []
 html_list = []
+a_list = []
 
 
 def url_load(u, hl):  # opens up index.html and then runs then appends html dat to htmlList
     f = urllib.request.urlopen(u)
     mf = f.read()
-    list.append(mf, hl)
-    return mf
+    h_soup = BeautifulSoup(mf, 'html.parser')
+    list.append(hl, h_soup)
+    return h_soup
 
 
 data = url_load(urls, html_list)
 
 
-soup = BeautifulSoup(data, 'html.parser')
+
 
 
 # cleans up the url to only include root domain url
@@ -26,13 +27,13 @@ soup = BeautifulSoup(data, 'html.parser')
 def domain(u):
     d = u
     if 'https://www.' in d:
-        u.replace('https://www.','')
+        u.replace('https://www.', '')
     elif 'http://www.' in d:
-        u.replace('http://www.','')
+        u.replace('http://www.', '')
     elif 'http://' in d:
-        u.replace('http://','')
+        u.replace('http://', '')
     elif 'https://' in d:
-        u.replace('https://','')
+        u.replace('https://', '')
     return d
 
 
@@ -42,19 +43,23 @@ dom = domain(urls)
 # 3. runs a check to see if the links in the page contain the domain
 # and then add that to aList
 # and then then adds then appends htmlList
-def all_links(aL, hl):
-    for link in soup.find_all('a'):
+def all_links(al, hl):
+    for link in data.find_all('a'):
         if dom in link:
             a = link.get('href')
-            list.append(a, aL)
-    for link in aL:
+            list.append(a, al)
+    for link in al:
         f = urllib.request.urlopen(link)
         mf = f.read()
-        list.append(mf, hl)
+        soup = BeautifulSoup(mf, 'html.parser')
+        list.append(hl, soup)
         return mf
 
 
+fin = all_links(a_list, html_list)
 
+print(a_list)
 
+print(html_list)
 
 
